dfb1cd410c4f388b53e8ed047759d31f07881bd6
talasago <42575556+talasago@users.noreply.github.com>
2023-03-26 22:34:20 +0900

    feat: add button to display other cat

--- survival_typescript/random-cat/pages/index.tsx
+++ survival_typescript/random-cat/pages/index.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 const IndexPage: NextPage = () => {
   const [imageUrl, setImageUrl] = useState("");
   const [loading, setLoading] = useState(true);
-
+  // NOTE:useEffectはasync関数を渡せない
   useEffect(() => {
     fetchImage().then((newImage) => {
       setImageUrl(newImage.url);
@@ -12,17 +12,28 @@ const IndexPage: NextPage = () => {
     });
   }, []);
 
-  return <div>{loading || <img src={imageUrl} />}</div>;
+  // ボタンをクリックしたときに画像を読み込む処理
+  const handleClick = async () => {
+    setLoading(true); // 読込中フラグを立てる
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url); // 画像URLの状態を更新する
+    setLoading(false); // 読込中フラグを倒す
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>他のにゃんこも見る</button>
+      <div>{loading || <img src={imageUrl} />}</div>
+    </div>
+  );
 };
 export default IndexPage;
 
 type Image = {
   url: string;
-}
-
+};
 const fetchImage = async (): Promise<Image> => {
   const res = await fetch("https://api.thecatapi.com/v1/images/search");
   const images = await res.json();
   console.log(images);
   return images[0];
-}
+};

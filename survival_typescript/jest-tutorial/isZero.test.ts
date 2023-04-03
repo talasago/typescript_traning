97c993609ce17adb5efe95b4c88d710a4f3378ac
talasago <42575556+talasago@users.noreply.github.com>
2023-04-03 21:39:08 +0900

    add test-case

--- survival_typescript/jest-tutorial/isZero.test.ts
+++ survival_typescript/jest-tutorial/isZero.test.ts
@@ -4,3 +4,8 @@ test('Be true if 0 is passed.',  () => {
   const result = isZero(0);
   expect(result).toBe(true);
 });
+
+test('Be false if 1 is passed.', () => {
+  const result = isZero(1);
+  expect(result).toBe(false);
+});

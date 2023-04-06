cd2c66556e18e205fa49f1db4392caa6a3631d56
talasago <42575556+talasago@users.noreply.github.com>
2023-04-06 20:29:54 +0900

    add test-content of SimpleButton

--- survival_typescript/component-test-tutorial/src/SimpleButton.test.tsx
+++ survival_typescript/component-test-tutorial/src/SimpleButton.test.tsx
@@ -1,8 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { SimpleButton } from "./SimpleButton";
 
-test("The display of ON/OFF change with click the button", () => {
-  render(<SimpleButton/ >);
+test("The display of ON/OFF change with click the button", async () => {
+  render(<SimpleButton />);
   const simpleButton = screen.getByRole("button");
   expect(simpleButton).toHaveTextContent("OFF");
+  userEvent.click(simpleButton);
+  // waitFor()を入れることでGreenになった。
+  // act()を入れるのでもいいのかもしれない。こちらは未確認
+  await waitFor(() => {
+    expect(simpleButton).toHaveTextContent("ON");
+  });
 });
